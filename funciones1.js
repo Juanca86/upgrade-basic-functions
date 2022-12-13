0efbd535c4373dc1c38e45677caa8063ad290ba4
Charly <plmallorca@gmail.com>
2022-12-13 16:38:23 +0100

    iteration#1

---
+++ funciones1.js
@@ -0,0 +1,16 @@
+function sum(numberOne , numberTwo) {
+    if (numberOne > numberTwo){
+        
+        console.log('el numero uno es mayor ' + numberOne)
+        
+    }
+    else if(numberOne == numberTwo) {
+        console.log('ambos numeros tienen el mismo valor')
+        
+    }
+    else {
+        numberTwo>numberOne
+        console.log('el numero dos es mayor ' + numberTwo );
+    }
+    }
+    sum (4, 5);
